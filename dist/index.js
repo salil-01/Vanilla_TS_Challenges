3eac1a7c47827d340358697fb52a1ad3f9c982c8
Salil Upadhyay <upsalil.01@gmail.com>
2025-02-03 00:43:49 +0530

    fix for home page

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,13 @@
 // src/index.ts
 const pageMap = ["my-christmas-list"];
 (() => {
-  const container = document.getElementById("#container");
-  if (container) {
-    pageMap.forEach((el) => {
-      const pageLink = document.createElement("a");
-      pageLink.href = el;
-      container.appendChild(pageLink);
-    });
-  }
+    const container = document.getElementById("container");
+    if (container) {
+        pageMap.forEach((el) => {
+            const pageLink = document.createElement("a");
+            pageLink.href = el;
+            pageLink.textContent = el === null || el === void 0 ? void 0 : el.split("-").join(" ");
+            container.appendChild(pageLink);
+        });
+    }
 })();
